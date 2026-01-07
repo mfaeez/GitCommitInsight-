7e646f34f00b8a84d096ce82b197824e500b15bb
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2026-01-07 22:44:35 +0530

    Update git_reader.py

--- src/core/git_reader.py
+++ src/core/git_reader.py
@@ -1,1 +1,26 @@
+import subprocess
+from datetime import datetime
+
+
+def read_commits(repo_path="."):
+    command = [
+        "git",
+        "log",
+        "--pretty=format:%H|%an|%ad|%s",
+        "--date=iso",
+    ]
+
+    result = subprocess.check_output(command, cwd=repo_path).decode()
+    commits = []
+
+    for line in result.splitlines():
+        commit_hash, author, date, message = line.split("|", 3)
+        commits.append({
+            "hash": commit_hash,
+            "author": author,
+            "date": datetime.fromisoformat(date),
+            "message": message,
+        })
+
+    return commits
 
